64c726b4d0d48972426ba62ae616849964048423
KennyDeblaere <kenny.deblaere@live.be>
2015-12-22 17:28:54 +0100

    Spinner, nog geen functionaliteit en fullscreen

--- FeedbackApp/app/src/main/java/be/howest/deblaere/kenny/feedbackapp/MainActivity.java
+++ FeedbackApp/app/src/main/java/be/howest/deblaere/kenny/feedbackapp/MainActivity.java
@@ -1,5 +1,6 @@
 package be.howest.deblaere.kenny.feedbackapp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    private Spinner courseSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        courseSpinner = (Spinner) findViewById(R.id.course_spinner);
+        fillSpinner();
+    }
 
+    private void fillSpinner(){
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.courses, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        courseSpinner.setAdapter(adapter);
     }
 }
