3e602626f3e2b1282eb7d07f178ebe56d522b1e7
KennyDeblaere <kenny.deblaere@live.be>
2015-12-22 18:58:19 +0100

    Event aan Spinner

--- FeedbackApp/app/src/main/java/be/howest/deblaere/kenny/feedbackapp/MainActivity.java
+++ FeedbackApp/app/src/main/java/be/howest/deblaere/kenny/feedbackapp/MainActivity.java
@@ -9,23 +9,42 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private Spinner courseSpinner;
+    private TextView courseTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         courseSpinner = (Spinner) findViewById(R.id.course_spinner);
+        courseTitle = (TextView) findViewById(R.id.selected);
         fillSpinner();
+        addListenerToSpinner();
     }
 
     private void fillSpinner(){
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.courses, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         courseSpinner.setAdapter(adapter);
+    }
+    private void addListenerToSpinner(){
+        courseSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                courseTitle.setText(courseSpinner.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
